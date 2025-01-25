5d0f499aa9be332ea07a8307b485b7a409191a47
ganesh chate <ganeshchate2004@gmail.com>
2025-01-25 23:22:11 +0530

    Update server.js
    findal code

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Failed to connect to MongoDB:", err));
 
